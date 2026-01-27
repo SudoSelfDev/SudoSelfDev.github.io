1e378faf24714820c537b4c34e9da0e014dee100
El Mehdi Azgaou <elmehdiazgaou@gmail.com>
2026-01-27 22:07:17 +0100

    adding darkmode feature

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+const toggleLink = document.getElementById('darkToggle');
+
+toggleLink.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  // 1. Toggle the class on the body
+  document.body.classList.toggle('dark-mode');
+
+  // 2. Check if dark-mode is now active
+  if (document.body.classList.contains('dark-mode')) {
+    // If it is dark, change link text to "Light Mode"
+    toggleLink.textContent = "☀️";
+  } else {
+    // If it's not dark, change link text back to "Dark Mode"
+    toggleLink.textContent = "🌙";
+  }
+});

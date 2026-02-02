a387d75500d4d076b4d6fd2c7541712a422266f7
El Mehdi Azgaou <elmehdiazgaou@gmail.com>
2026-02-02 22:31:23 +0100

    styling ObsidianSyncGuide

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,13 @@
-const toggleLink = document.getElementById('darkToggle');
+const toggleLink = document.getElementById("darkToggle");
 
-toggleLink.addEventListener('click', function(event) {
+toggleLink.addEventListener("click", function (event) {
   event.preventDefault();
 
   // 1. Toggle the class on the body
-  document.body.classList.toggle('dark-mode');
+  document.body.classList.toggle("dark-mode");
 
   // 2. Check if dark-mode is now active
-  if (document.body.classList.contains('dark-mode')) {
+  if (document.body.classList.contains("dark-mode")) {
     // If it is dark, change link text to "Light Mode"
     toggleLink.textContent = "☀️";
   } else {
